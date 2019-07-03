6dbb12f22025fb40f4e0cdc9b95f5d968fd63324
Char <lucaschew0@gmail.com>
2019-07-03 10:04:49 +1000

    V2

---
+++ V2/ai.c
@@ -0,0 +1,589 @@
+////////////////////////////////////////
+//
+//	File : ai.c
+//	CoSpace Robot
+//	Version 1.0.0
+//	Jan 1 2016
+//	Copyright (C) 2016 CoSpace Robot. All Rights Reserved
+//
+//////////////////////////////////////
+//
+// ONLY C Code can be compiled.
+//
+/////////////////////////////////////
+
+#define CsBot_AI_H//DO NOT delete this line
+#ifndef CSBOT_REAL
+#include <windows.h>
+#include <stdio.h>
+#include <math.h>
+#define DLL_EXPORT extern __declspec(dllexport)
+#define false 0
+#define true 1
+#endif//The robot ID : It must be two char, such as '00','kl' or 'Cr'.
+char AI_MyID[2] = {'0','2'};
+
+int Duration = 0;
+int SuperDuration = 0;
+int bGameEnd = false;
+int CurAction = -1;
+int CurGame = 0;
+int SuperObj_Num = 0;
+int SuperObj_X = 0;
+int SuperObj_Y = 0;
+int Teleport = 0;
+int LoadedObjects = 0;
+int xHold = -1;
+int yHold = -1;
+int US_Front = 0;
+int US_Left = 0;
+int US_Right = 0;
+int CSLeft_R = 0;
+int CSLeft_G = 0;
+int CSLeft_B = 0;
+int CSRight_R = 0;
+int CSRight_G = 0;
+int CSRight_B = 0;
+int PositionX = 0;
+int PositionY = 0;
+int TM_State = 0;
+int Compass = 0;
+int Time = 0;
+int WheelLeft = 0;
+int WheelRight = 0;
+int LED_1 = 0;
+int MyState = 0;
+int AI_SensorNum = 13;
+
+#define CsBot_AI_C//DO NOT delete this line
+
+DLL_EXPORT void SetGameID(int GameID)
+{
+    CurGame = GameID;
+    bGameEnd = 0;
+}
+
+DLL_EXPORT int GetGameID()
+{
+    return CurGame;
+}
+
+//Only Used by CsBot Dance Platform
+DLL_EXPORT int IsGameEnd()
+{
+    return bGameEnd;
+}
+
+#ifndef CSBOT_REAL
+
+DLL_EXPORT char* GetDebugInfo()
+{
+    char info[1024];
+    sprintf(info, "Duration=%d;SuperDuration=%d;bGameEnd=%d;CurAction=%d;CurGame=%d;SuperObj_Num=%d;SuperObj_X=%d;SuperObj_Y=%d;Teleport=%d;LoadedObjects=%d;xHold=%d;yHold=%d;US_Front=%d;US_Left=%d;US_Right=%d;CSLeft_R=%d;CSLeft_G=%d;CSLeft_B=%d;CSRight_R=%d;CSRight_G=%d;CSRight_B=%d;PositionX=%d;PositionY=%d;TM_State=%d;Compass=%d;Time=%d;WheelLeft=%d;WheelRight=%d;LED_1=%d;MyState=%d;",Duration,SuperDuration,bGameEnd,CurAction,CurGame,SuperObj_Num,SuperObj_X,SuperObj_Y,Teleport,LoadedObjects,xHold,yHold,US_Front,US_Left,US_Right,CSLeft_R,CSLeft_G,CSLeft_B,CSRight_R,CSRight_G,CSRight_B,PositionX,PositionY,TM_State,Compass,Time,WheelLeft,WheelRight,LED_1,MyState);
+    return info;
+}
+ 
+DLL_EXPORT char* GetTeamName()
+{
+     return "SAC SIM";
+}
+
+DLL_EXPORT int GetCurAction()
+{
+    return CurAction;
+}
+
+//Only Used by CsBot Rescue Platform
+DLL_EXPORT int GetTeleport()
+{
+    return Teleport;
+}
+
+//Only Used by CsBot Rescue Platform
+DLL_EXPORT void SetSuperObj(int X, int Y, int num)
+{
+    SuperObj_X = X;
+    SuperObj_Y = Y;
+    SuperObj_Num = num;
+}
+//Only Used by CsBot Rescue Platform
+DLL_EXPORT void GetSuperObj(int *X, int *Y, int *num)
+{
+    *X = SuperObj_X;
+    *Y = SuperObj_Y;
+    *num = SuperObj_Num;
+}
+
+#endif ////CSBOT_REAL
+
+DLL_EXPORT void SetDataAI(volatile int* packet, volatile int *AI_IN)
+{
+
+    int sum = 0;
+
+    US_Front = AI_IN[0]; packet[0] = US_Front; sum += US_Front;
+    US_Left = AI_IN[1]; packet[1] = US_Left; sum += US_Left;
+    US_Right = AI_IN[2]; packet[2] = US_Right; sum += US_Right;
+    CSLeft_R = AI_IN[3]; packet[3] = CSLeft_R; sum += CSLeft_R;
+    CSLeft_G = AI_IN[4]; packet[4] = CSLeft_G; sum += CSLeft_G;
+    CSLeft_B = AI_IN[5]; packet[5] = CSLeft_B; sum += CSLeft_B;
+    CSRight_R = AI_IN[6]; packet[6] = CSRight_R; sum += CSRight_R;
+    CSRight_G = AI_IN[7]; packet[7] = CSRight_G; sum += CSRight_G;
+    CSRight_B = AI_IN[8]; packet[8] = CSRight_B; sum += CSRight_B;
+    PositionX = AI_IN[9]; packet[9] = PositionX; sum += PositionX;
+    PositionY = AI_IN[10]; packet[10] = PositionY; sum += PositionY;
+    TM_State = AI_IN[11]; packet[11] = TM_State; sum += TM_State;
+    Compass = AI_IN[12]; packet[12] = Compass; sum += Compass;
+    Time = AI_IN[13]; packet[13] = Time; sum += Time;
+    packet[14] = sum;
+
+}
+DLL_EXPORT void GetCommand(int *AI_OUT)
+{
+    AI_OUT[0] = WheelLeft;
+    AI_OUT[1] = WheelRight;
+    AI_OUT[2] = LED_1;
+    AI_OUT[3] = MyState;
+}
+void Game0()
+{
+
+    if(SuperDuration>0)
+    {
+        SuperDuration--;
+    }
+    else if(Duration>0)
+    {
+        Duration--;
+    }
+    else if(Time>=300 && Time<=1000)
+    {
+        Duration = 0;
+        CurAction =1;
+    }
+    else if(((CSLeft_B>=200&&CSLeft_G<=70&&CSLeft_R<=70)||(CSRight_B>=200&&CSRight_G<=70&&CSRight_R<=70))
+
+)
+    {
+        Duration = 1;
+        CurAction =2;
+    }
+    else if(((CSLeft_R>200&&CSLeft_G>200&&CSLeft_B<10)||(CSRight_R>200&&CSRight_G>200&&CSRight_B<10)))
+    {
+        Duration = 8;
+        CurAction =3;
+    }
+    else if((((CSLeft_R>200&&CSLeft_G>=160&&CSLeft_B<10)&&(CSRight_R>200&&CSRight_G>=160&&CSRight_B<10))&&LoadedObjects>0))
+    {
+        Duration = 59;
+        CurAction =4;
+    }
+    else if(((CSLeft_B<=50&&CSLeft_G<=50&&CSLeft_R<=50)||(CSRight_B<=50&&CSRight_G<=50&&CSRight_R<=50))&&LoadedObjects<6)
+    {
+        Duration = 49;
+        CurAction =5;
+    }
+    else if(((CSLeft_B>=230&&CSLeft_G>=230&&CSLeft_R<=50)||(CSRight_B>=230&&CSRight_G>=230&&CSRight_R<=50))&&LoadedObjects<6
+)
+    {
+        Duration = 49;
+        CurAction =6;
+    }
+    else if(((CSLeft_R>220&&CSLeft_G<90&&CSLeft_B<90)||(CSRight_R>220&&CSRight_B<90&&CSRight_G<90))&&LoadedObjects<6)
+    {
+        Duration = 49;
+        CurAction =7;
+    }
+    else if(US_Front>=0 && US_Front<=10)
+    {
+        Duration = 0;
+        CurAction =8;
+    }
+    else if(true)
+    {
+        Duration = 0;
+        CurAction =9;
+    }
+    switch(CurAction)
+    {
+        case 1:
+            WheelLeft=0;
+            WheelRight=0;
+            LED_1=0;
+            MyState=0;
+             Teleport =3;  
+            LoadedObjects = 0;
+            WheelLeft = 0;  WheelRight = 0;  LED_1=0;
+            Duration = 0;   SuperDuration = 0;
+            break;
+        case 2:
+            WheelLeft=5;
+            WheelRight=5;
+            LED_1=0;
+            MyState=0;
+            LoadedObjects=0;
+                    
+            break;
+        case 3:
+            WheelLeft=-2;
+            WheelRight=1;
+            LED_1=0;
+            MyState=0;
+            if (Duration < 5){
+
+WheelLeft = -2;
+                    
+WheelRight = -2;
+                    
+
+}else if (US_Right > 5){
+WheelLeft = 3;
+                    
+WheelRight = -1;
+                    
+}else{
+
+WheelLeft = -1;
+                    
+WheelRight = 3;
+                    
+
+}
+
+
+
+
+
+
+            break;
+        case 4:
+            WheelLeft=0;
+            WheelRight=0;
+            LED_1=2;
+            MyState=0;
+            if(Duration==1){
+LoadedObjects=0;
+                    
+}
+if(Duration==60){
+WheelLeft=-5;
+                    
+WheelRight=-5;
+                    
+}
+            if(Duration == 1) {LoadedObjects = 0; } 
+            break;
+        case 5:
+            WheelLeft=0;
+            WheelRight=0;
+            LED_1=1;
+            MyState=0;
+            if(Duration == 1) LoadedObjects++;
+            if(Duration < 6)
+            {
+                WheelLeft = 2;
+                WheelRight = 2;
+            }
+            break;
+        case 6:
+            WheelLeft=0;
+            WheelRight=0;
+            LED_1=1;
+            MyState=0;
+            if(Duration == 1) LoadedObjects++;
+            if(Duration < 6)
+            {
+                WheelLeft = 2;
+                WheelRight = 2;
+            }
+            break;
+        case 7:
+            WheelLeft=0;
+            WheelRight=0;
+            LED_1=1;
+            MyState=0;
+            if(Duration == 1) LoadedObjects++;
+            if(Duration < 6)
+            {
+                WheelLeft = 2;
+                WheelRight = 2;
+            }
+            break;
+        case 8:
+            WheelLeft=0;
+            WheelRight=0;
+            LED_1=0;
+            MyState=0;
+            if (US_Right >= US_Left){
+
+WheelLeft = 3;
+                    
+WheelRight = -1;
+                    
+
+}
+else {
+
+WheelRight = 3;
+                    
+WheelLeft = -1;
+                    
+
+}
+            break;
+        case 9:
+            WheelLeft=2;
+            WheelRight=2;
+            LED_1=0;
+            MyState=0;
+            if (LoadedObjects >= 6){
+WheelLeft = 4;
+                    
+WheelRight = 4;
+                    
+}
+            break;
+        default:
+            break;
+    }
+
+}
+
+void Game1()
+{
+
+    if(SuperDuration>0)
+    {
+        SuperDuration--;
+    }
+    else if(Duration>0)
+    {
+        Duration--;
+    }
+    else if(((PositionX<=5)||(PositionX>=350)||
+(PositionY<=5)||(PositionY>=260))
+
+&&
+
+(PositionX!=0&&PositionY!=0))
+    {
+        Duration = 128;
+        CurAction =1;
+    }
+    else if(LoadedObjects==6&&xHold!=-1&&yHold!=-1)
+    {
+        Duration = 128;
+        CurAction =2;
+    }
+    switch(CurAction)
+    {
+        case 1:
+            WheelLeft=0;
+            WheelRight=0;
+            LED_1=0;
+            MyState=0;
+            if (PositionX<=5 && Compass >= 280 || Compass <= 260){
+   WheelLeft = -1;
+                    
+   WheelRight = 1;
+                    
+
+}
+else if (PositionX>=350 && Compass >= 100 || Compass <= 80)	{
+
+		WheelLeft = -1;
+                    
+		WheelRight = 1;
+                    
+	
+}
+else if (PositionY<=5 && Compass >= 10 && Compass <= 350)	{
+		WheelLeft = -1;
+                    
+		WheelRight = 1;
+                    
+	
+}
+else if (PositionY>=260 && Compass >= 190 || Compass <= 170)	{
+		WheelLeft = -1;
+                    
+		WheelRight = 1;
+                    
+	
+}
+else {
+
+	WheelLeft = 3;
+                    
+	WheelRight = 3;
+                    
+
+}
+ Duration = 128;
+            if(((PositionX>=5)&&(PositionX<=350)&&
+(PositionY>=5)&&(PositionY<=260))
+
+&&
+
+(PositionX!=0&&PositionY!=0)
+) Duration=0;
+            break;
+        case 2:
+            WheelLeft=0;
+            WheelRight=0;
+            LED_1=0;
+            MyState=0;
+            /*
+		 0
+		 |
+		 |
+  90 - - + - - 270
+		 |
+		 |
+		180
+
+
+*/
+
+
+printf("init");
+                    
+
+if (PositionX > xHold && PositionY > yHold && Compass >= 145 && Compass <= 125){
+
+	WheelLeft = -1;
+                    
+	WheelRight = 1;
+                    
+
+//Left, Down
+}
+else if (PositionX > xHold && PositionY < yHold && Compass >= 55 && Compass <= 35)	{
+
+	WheelLeft = -1;
+                    
+	WheelRight = 1;
+                    
+
+//Left, Up
+}
+else if (PositionX < xHold && PositionY > yHold && Compass >= 235 && Compass <= 215){
+
+	WheelLeft = -1;
+                    
+	WheelRight = 1;
+                    
+
+//Right, Down
+}
+else if (PositionX < xHold && PositionY < yHold && Compass >= 325 && Compass <= 305)	{
+
+	WheelLeft = -1;
+                    
+	WheelRight = 1;
+                    
+
+//Right, Up
+}
+
+
+//------------------------------------------------------------------------------
+
+else if (PositionX > xHold && Compass >= 100 && Compass <= 80)	{
+
+	WheelLeft = -1;
+                    
+	WheelRight = 1;
+                    	
+
+//Left
+}
+else if (PositionX < xHold && Compass >= 280 && Compass <= 260) {
+
+	WheelLeft = -1;
+                    
+	WheelRight = 1;
+                    
+
+//Right
+}
+else if (PositionY > yHold && Compass >= 190 && Compass <= 170)	{
+
+	WheelLeft = -1;
+                    
+	WheelRight = 1;
+                    
+	
+//Down
+}
+else if (PositionY < yHold && Compass >= 10 && Compass <= 350)	{
+
+	WheelLeft = -1;
+                    
+	WheelRight = 1;
+                    
+
+//Up
+}
+else 	{
+
+	WheelLeft = 3;
+                    
+	WheelRight = 3;
+                    
+
+}
+
+
+
+
+ Duration = 128;
+            if((US_Front<10)
+//Distance
+
+||
+
+((CSLeft_R>200&&CSLeft_G>200&&CSLeft_B<10)||(CSRight_R>200&&CSRight_G>200&&CSRight_B<10))
+//Avoid
+
+||
+
+((CSLeft_R>230&&CSLeft_G>230&&CSLeft_B>10&&CSLeft_B<70)||(CSRight_R>230&&CSRight_G>230&&CSRight_B>10&&CSRight_B<70))
+//Outside
+
+||
+
+(((CSLeft_R>200&&CSLeft_G>=160&&CSLeft_B<10)&&(CSRight_R>200&&CSRight_G>=160&&CSRight_B<10))&&LoadedObjects>0)
+//Deposit
+) Duration=0;
+            break;
+        default:
+            break;
+    }
+
+}
+
+
+DLL_EXPORT void OnTimer()
+{
+    switch (CurGame)
+    {
+        case 9:
+            break;
+        case 10:
+            WheelLeft=0;
+            WheelRight=0;
+            LED_1=0;
+            MyState=0;
+            break;
+        case 0:
+            Game0();
+            break;
+        case 1:
+            Game1();
+            break;
+        default:
+            break;
+    }
+}
+
